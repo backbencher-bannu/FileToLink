2c6043871225d7a8ecdbc33ca7ebe0bc6866209e
backbencher-bannu <harishwhatsapp0306@gmail.com>
2025-01-23 17:54:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,5 +6,5 @@ BOT_TOKEN = os.getenv('BOT_TOKEN', "7361998653:AAEpt3jH-FpJMJR7ILSeem7mH4SgIr9k8
 
 # stream vars
 PORT = int(os.getenv('PORT', '5050'))
-BIN_CHANNEL = os.getenv("BIN_CHANNEL", -1002185509993"") #Log Channel
+BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002185509993") #Log Channel
 URL = os.getenv("URL", "mongodb+srv://harishkumargorinta:P6ak9kZKvx33Jx3@movies24.ng58m.mongodb.net/?retryWrites=true&w=majority&appName=Movies24") #App URL not MongoDB URL
